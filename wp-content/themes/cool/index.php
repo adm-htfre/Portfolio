de6672deaa4dca86a949cb14d483213917159c2e
Pranjal <adm.htfre@gmail.com>
2020-05-06 12:40:29 +0530

    new theme

---
+++ wp-content/themes/cool/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Solid State by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<?php get_header(); ?>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="generic.html">Generic</a></li>
+								<li><a href="elements.html">Elements</a></li>
+								<li><a href="#">Log In</a></li>
+								<li><a href="#">Sign Up</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"><span class="icon fa-gem"></span></div>
+							<h2>This is Solid State</h2>
+							<p>Another free + fully responsive site template by <a href="http://html5up.net">HTML5 UP</a></p>
+						</div>
+					</section>
+
+				<!-- Wrapper -->
+					<section id="wrapper">
+<?php if ( have_posts() ) : ?>
+<?php while ( have_posts() ) : the_post(); ?>
+  <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="post-header">
+       <div class="date"><?php the_time( 'M j y' ); ?></div>
+       <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+       <div class="author"><?php the_author(); ?></div>
+    </div><!--end post header-->
+    <div class="entry clear">
+       <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
+       <?php the_content(); ?>
+       <?php edit_post_link(); ?>
+       <?php wp_link_pages(); ?> </div>
+    <!--end entry-->
+    <div class="post-footer">
+       <div class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
+    </div><!--end post footer-->
+    </div><!--end post-->
+<?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
+    <div class="navigation index">
+       <div class="alignleft"><?php next_posts_link( 'Older Entries' ); ?></div>
+       <div class="alignright"><?php previous_posts_link( 'Newer Entries' ); ?></div>
+    </div><!--end navigation-->
+<?php else : ?>
+<?php endif; ?>
+
+					</section>
+					
+					<? php get_footer(); ?>
